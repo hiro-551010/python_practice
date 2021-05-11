839eec4dc7fa14b138257668250ab6046e1ab47f
hiro-551010 <hiro551010@gmail.com>
2021-05-11 14:04:43 +0900

    課題0

---
+++ task0.py
@@ -0,0 +1,40 @@
+
+# --- 1 ---
+name1 = 'ねずこ'
+name2 = 'ぜんいつ'
+print('{}と{}は仲間です'.format(name1, name2))
+
+# --- 2 ---
+name2 = 'むざん'
+print('{}と{}は仲間ではありません'.format(name1, name2))
+
+# --- 3 ---
+name = ["たんじろう","ぎゆう","ねずこ","むざん"]
+name.append('ぜんいつ')
+
+# --- 4 ---
+for obj in name:
+    print(obj)
+
+# --- 5 ---
+def hello():
+    print('hello')
+
+hello()
+
+# --- 6 ---
+def character(character):
+    if character in name:
+        print('{}はnameに含まれます'.format(character))
+    else:
+        print('{}はnameには含まれていません'.format(character))
+
+character('ねずこ')
+
+
+
+
+
+
+
+
