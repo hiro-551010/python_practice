db23df92bf99439c7e0bc4ad5176a166eead82bd
hiro-551010 <hiro551010@gmail.com>
2021-05-12 12:15:51 +0900

    課題1-1

--- task1.py
+++ task1.py
@@ -3,16 +3,18 @@ path = './task1.csv'
 
 def search():
     file = pd.read_csv(path)
-    source = list(file['name'])
-    name = input('名前を入れてください')
-    if name in source:
-        print('{}を見つけました'.format(name))
-        print('source')
-    else:
-        source.append(name)
-        print('{}を保存しました'.format(name))
-        name_df = pd.DataFrame(data=source, columns=['name'])
-        name_df.to_csv(path, encoding="utf-8")
-        print(source)
+    if 'name' in file.columns:
+        source = list(file['name'])
+        name = input('名前を入れてください')
+        if name in source:
+            print('{}を見つけました'.format(name))
+            print('source')
+        else:
+            source.append(name)
+            print('{}を保存しました'.format(name))
+            name_df = pd.DataFrame(data=source, columns=['name'])
+            name_df.to_csv(path, encoding="utf-8")
+            print(source)
 
 search()
+
