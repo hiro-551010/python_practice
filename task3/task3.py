42eb6915baada77fbe2176284c0154e8aff5e5fb
hiro-551010 <hiro551010@gmail.com>
2021-05-30 14:43:20 +0900

    csvファイルの読み込みがしたい

--- task3/task3.py
+++ task3/task3.py
@@ -1,11 +1,12 @@
 import eel
 import pandas as pd
 
+
 @eel.expose
-def search(name):
-    path = './characters.csv'
+def search(name, csv_name):
+    path = './{}'.format(csv_name)
     file = pd.read_csv(path)
-    if 'name' in file.columns:
+    if name in file.columns:
         source = list(file['name'])
         # name = input('名前を入れてください')
         if name in source:
