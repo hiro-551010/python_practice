41af4e374ca2df8d9aaeb649529e3df7b4bc232c
hiro-551010 <hiro551010@gmail.com>
2021-06-04 03:57:40 +0900

    eel.exposeがキモ？

--- task3/task3.py
+++ task3/task3.py
@@ -5,17 +5,19 @@ import os
 
 
 def search(name, csv_name):
-    path = os.path.join(os.path.dirname(__file__),csv_name)
+    path = os.path.join(os.path.dirname(__file__), csv_name)
     file = pd.read_csv(path)
     if name in file.columns:
         source = list(file['name'])
         # name = input('名前を入れてください')
         if name in source:
+            print('{}を見つけました'.format(name)), name
             return print('{}を見つけました'.format(name)), name
         else:
             source.append(name)
             name_df = pd.DataFrame(data=source, columns=['name'])
             name_df.to_csv(path, encoding="utf-8")
+            
             return print('{}を保存しました'.format(name)), name
 
 
